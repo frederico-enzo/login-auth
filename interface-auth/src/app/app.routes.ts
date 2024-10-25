36971e9f15afebbeda23e62dadfaeca9f2861e26
Frederico <frederico.enzo@hotmail.com>
2024-10-25 16:00:25 -0300

    chrome: configuraçao de rotas para aceitar somente login e register para liberar o index

--- interface-auth/src/app/app.routes.ts
+++ interface-auth/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { IndexViewComponent } from './views/componentes/index-view/index-view.component';
+import { LoginViewComponent } from './views/manager/login-view/login-view.component';
+import { NgModule } from '@angular/core';
+import { RegisterViewComponent } from './views/manager/register-view/register-view.component';
+import { rotaGuard } from './guard/rota-guard.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "login", pathMatch: 'full' },
+    { path: "register", component: RegisterViewComponent },
+    { path: "login", component: LoginViewComponent },
+    {
+      path: "admin", component: IndexViewComponent, canActivate: [rotaGuard], children: [
+  
+      ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
