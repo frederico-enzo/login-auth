808af4d954f72a7447383855379d2135f0338821
Frederico <frederico.enzo@hotmail.com>
2024-10-25 16:01:53 -0300

    fix: delete service exclusiva de user e metodos de login-register e validar se o tokken esta no localStorage

---
+++ interface-auth/src/app/views/componentes/index-view/index-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './index-view.component.html',
+  styleUrl: './index-view.component.css'
+})
+export class IndexViewComponent {
+
+}
