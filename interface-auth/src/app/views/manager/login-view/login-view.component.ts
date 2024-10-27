6b056bbc3c5e566b5ea7f630020323d6ba1ea35a
Frederico <frederico.enzo@hotmail.com>
2024-10-26 21:55:24 -0300

    feat: tela de login; (Há erroa a ser revisados

--- interface-auth/src/app/views/manager/login-view/login-view.component.ts
+++ interface-auth/src/app/views/manager/login-view/login-view.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoginService } from '../../../service/login-service/login.service';
+import { Login } from '../../../model/login-model/login';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-view',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-view.component.html',
   styleUrl: './login-view.component.css'
 })
 export class LoginViewComponent {
+  loginService = inject(LoginService);
+  login: Login = new Login();
+  roteador = inject(Router);
 
+  constructor() {
+    this.loginService.removeToken();
+  }
+
+  logar() {
+    this.loginService.logar(this.login).subscribe({
+      next: usuario => {localStorage.setItem("token", usuario.token),this.roteador.navigate(['admin/produtos']);
+    },
+      error: erro => {console.log(erro);
+      }
+    });
+
+   
+  }
 }
